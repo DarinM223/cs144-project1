0685cd919d70d32b94a321bb6ec24ff79ea0d737
Darin Minamoto <darinm223@gmail.com>
2015-01-05 19:36:46 -0800

    Initial Commit

---
+++ src/ComputeSHA.java
@@ -0,0 +1,16 @@
+/**
+ * Computes the SHA-1 hash over the content of an input file
+ * @author Darin Minamoto <darinm223@gmail.com>
+ * 
+ * <pre>
+ * {@code
+ * java ComputeSHA filename.txt
+ * -> 17a23c746fed66a6f285c665422deafcf51aca40
+ * }
+ * </pre>
+ */
+public class ComputeSHA {
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
